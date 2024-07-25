c2cc89c8af35a87263e7c8525c7b5843aa89e528
paiphyonaing93 <paiphyonaing93@gmail.com>
2024-07-25 19:32:11 +0700

    message

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -48,3 +48,8 @@ switch (userType) {
 }
 
 console.log("User Category: ", userCategory);
+
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not Authenticated";
+
+console.log("Authentication Status: ", authenticationStatus);
